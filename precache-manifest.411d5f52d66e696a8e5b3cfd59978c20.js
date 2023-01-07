9c0579c346f27fd7227e9c3db82d4a1521aa72de
vishnu-1993-developer <64806791+vishnu-1993-developer@users.noreply.github.com>
2023-01-08 03:10:18 +0530

    My-text-utils-app-is-ready

---
+++ precache-manifest.411d5f52d66e696a8e5b3cfd59978c20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "575bc6a5ab23da58a779",
+    "url": "/static/css/main.17765493.chunk.css"
+  },
+  {
+    "revision": "575bc6a5ab23da58a779",
+    "url": "/static/js/main.575bc6a5.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "bc97bed3ced8a15d5d83",
+    "url": "/static/js/2.bc97bed3.chunk.js"
+  },
+  {
+    "revision": "1458782b48af82eb3a96",
+    "url": "/static/js/runtime~main.1458782b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e3079b43c7b5910b3470ee28111e56ce",
+    "url": "/index.html"
+  }
+];
