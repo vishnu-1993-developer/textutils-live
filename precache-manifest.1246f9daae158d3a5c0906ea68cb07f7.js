637977e30088bd9f4ba92fb511be560e418d32c5
vishnu <vishnudev40@gmail.com>
2023-01-09 11:39:24 +0530

    bug fixes for new line

---
+++ precache-manifest.1246f9daae158d3a5c0906ea68cb07f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f19ede8a71524af741c1",
+    "url": "/static/css/main.fe99c75a.chunk.css"
+  },
+  {
+    "revision": "f19ede8a71524af741c1",
+    "url": "/static/js/main.f19ede8a.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "7a42820834e650b5ab11",
+    "url": "/static/js/2.7a428208.chunk.js"
+  },
+  {
+    "revision": "0e85d9ea22f618c5f874",
+    "url": "/static/js/runtime~main.0e85d9ea.js"
+  },
+  {
+    "revision": "7f51bcdd04b9b788d30c4dd2763d085b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cda3f81f530f2e4c8530dcc38ff439c2.js"
+  "/precache-manifest.1246f9daae158d3a5c0906ea68cb07f7.js"
 );
 
 workbox.clientsClaim();
